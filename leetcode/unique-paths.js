48b30e9147ca14c08cfdb2799b079d008fb08d46
chenxingliang <869997202@qq.com>
2019-12-25 17:41:04 +0800

    增加 增加unique-path

---
+++ leetcode/unique-paths.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+    let result = new Array(n).fill(1);
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            result[j] += result[j - 1];
+        }
+    }
+    return result[n - 1];
+};

a33182df11a65656cf049c71986c02bfbb0e4668
chenxingliang <869997202@qq.com>
2019-12-30 11:47:40 +0800

    修改 修改maximum-depth-of-binary-tree

--- leetcode/maximum-depth-of-binary-tree.js
+++ leetcode/maximum-depth-of-binary-tree.js
@@ -10,5 +10,13 @@
  * @return {number}
  */
 var maxDepth = function (root) {
+    let depth = 0;
 
+    if (root == null) {
+        return depth;
+    } 
+
+    depth = Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+
+    return depth;
 };

d52e461d91104682ca2ddd45ef9f57e3c69325a8
chenxingliang <869997202@qq.com>
2020-01-06 17:02:16 +0800

    修改 修改sort-list

--- leetcode/sort-list.js
+++ leetcode/sort-list.js
@@ -10,5 +10,48 @@
  * @return {ListNode}
  */
 var sortList = function (head) {
-
+    return head == null ? null : mergeList(head);
 };
+
+var mergeList = function (head) {
+    if (head.next == null) {
+        return head;
+    }
+
+    let slow = head,
+        fast = head,
+        pre = null;
+
+    while (fast && fast.next) {
+        pre = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    pre.next = null;
+
+    let left = mergeList(head),
+        right = mergeList(slow);
+    
+    return merge(left, right);
+}
+
+var merge = function (l1, l2) {
+    let res = new ListNode(),
+        current = res;
+
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+    current.next = l1 ? l1 : l2;
+
+    return res.next;
+}

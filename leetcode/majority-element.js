8e2c7b53388b414581888fe6ee4e28433209c0ab
chenxingliang <869997202@qq.com>
2020-01-15 09:38:23 +0800

    修改 修改majority-element.js

--- leetcode/majority-element.js
+++ leetcode/majority-element.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-    
+    let len = nums.length;
+
+    if (len == 1) {
+        return nums[0];
+    }
+
+    let hashMap = {},
+        majorityNum = null;
+
+    for (let num of nums) {
+        if (hashMap[num]) {
+            hasMap[num]++;
+            majorityNum = hasMap[num] > hasMap[majorityNum] ? num : majorityNum;
+        } else {
+            hasMap[num] = 1;
+        }
+    }
+
+    return majorityNum;
 };

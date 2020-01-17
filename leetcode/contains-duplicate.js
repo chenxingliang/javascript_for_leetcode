8693a83ed1217cecd61396e579137ac598523eb7
chenxingliang <869997202@qq.com>
2020-01-17 15:40:15 +0800

    增加 增加contains-duplicate

---
+++ leetcode/contains-duplicate.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    let hash = {};
+
+    for (let num of nums) {
+        if (hash[num]) {
+            return true;
+        } else {
+            hash[num] = true;
+        }
+    }
+
+    return false;
+};

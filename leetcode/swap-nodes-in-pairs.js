3aa9144f3c49dd484ce7e3cb5ecae736b2fcdbcf
chenxl <869997202@qq.com>
2020-03-14 20:28:18 +0800

    swap-nodes-in-pairs

--- leetcode/swap-nodes-in-pairs.js
+++ leetcode/swap-nodes-in-pairs.js
@@ -10,5 +10,21 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
+    if (head === null || head.next === null) {
+        return head;
+    }
 
+    let newHead = new ListNode(),
+        prev = newHead;
+
+    while (head !== null && head.next !== null) {
+        prev.next = head.next;
+        head.next = prev.next.next;
+        prev.next.next = head;
+
+        prev = head;
+        head = head.next;
+    }
+
+    return newHead.next;
 };

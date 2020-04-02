c64db9ad31c4c7ff50b5c7d1e064aabd3f64f8d6
chenxingliang <869997202@qq.com>
2020-04-02 18:16:57 +0800

    game-of-life

---
+++ leetcode/game-of-life.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var gameOfLife = function(board) {
+    let dx = [-1, 0, 1, -1, 1, -1, 0, 1],
+        dy = [-1, -1, -1, 0, 0, 1, 1, 1],
+        rLen = board.length,
+        cLen = board[0].length;
+    
+    
+
+    
+};

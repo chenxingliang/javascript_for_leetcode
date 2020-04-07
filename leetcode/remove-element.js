291b5d55fb23aef8d7dd29cb8eafe0dd285e1175
chenxingliang <869997202@qq.com>
2020-04-07 14:01:13 +0800

    remove-element

--- leetcode/remove-element.js
+++ leetcode/remove-element.js
@@ -4,5 +4,17 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
+    let i = 0,
+        len = nums.length;
 
+    while (i < len) {
+        if (nums[i] == val) {
+            [nums[i], nums[n - 1]] = [nums[n - 1], nums[i]];
+            n--;
+        } else {
+            i++;
+        }
+    }
+
+    return n;
 };

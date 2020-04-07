38f1f8a9d1d9ba897487a511b375b9a59d886d0b
chenxingliang <869997202@qq.com>
2020-04-07 10:16:26 +0800

    game-of-life

--- leetcode/game-of-life.js
+++ leetcode/game-of-life.js
@@ -7,8 +7,39 @@ var gameOfLife = function(board) {
         dy = [-1, -1, -1, 0, 0, 1, 1, 1],
         rLen = board.length,
         cLen = board[0].length;
-    
-    
 
+    function checkCell(r, c) {
+        if (0 <= r && r < rLen && 0 <= c && c < cLen && Math.abs(board[r][c]) == 1) {
+            return 1;
+        }
+
+        return 0;
+    }
     
+    for (let r = 0; r < rLen; r++) {
+        for (let c = 0; c < cLen; c++) {
+            let liveCells = 0;
+            for (i = 0; i < 8; i++) {
+                liveCells += checkCell(r + dy[i], c + dx[i]);
+            }
+            if (board[r][c] == 1 && (liveCells < 2 || liveCells > 3)) {
+                board[r][c] = -1;
+            }
+            if (board[r][c] == 0 && liveCells == 3) {
+                board[r][c] = 2;
+            }
+        }
+    }
+
+    for (let r = 0; r < rLen; r++) {
+        for (let c = 0; c < cLen; c++) {
+            if (board[r][c] > 0) {
+                board[r][c] = 1;
+            } else {
+                board[r][c] = 0;
+            }
+        }
+    }
 };
+
+let test = gameOfLife([[0,1,0],[0,0,1],[1,1,1],[0,0,0]]);

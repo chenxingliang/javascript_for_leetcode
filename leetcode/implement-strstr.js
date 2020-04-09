696f721c1ce205a1b887500b9e305366ca6d3b02
chenxingliang <869997202@qq.com>
2020-04-09 10:33:38 +0800

    增加implement-strstr

--- leetcode/implement-strstr.js
+++ leetcode/implement-strstr.js
@@ -4,5 +4,35 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
+    let hLen = haystack.length,
+        nLen = needle.length;
 
+    if (nLen == 0) {
+        return 0;
+    }
+
+    if (hLen < nLen) {
+        return -1;
+    }
+
+    let haystackPointer = 0,
+        needlePointer = 0;
+    
+    while (haystackPointer < hLen && needlePointer < nLen) {
+        if (haystack[haystackPointer] == needle[needlePointer]) {
+            if (needlePointer == nLen - 1) {
+                return haystackPointer - needlePointer;
+            }
+            haystackPointer++;
+            needlePointer++;
+        } else {
+            haystackPointer = haystackPointer - needlePointer + 1;
+            needlePointer = 0;
+        }
+    }
+
+    return -1;
 };
+
+let test = strStr("mississippi","issip");
+console.log(test);

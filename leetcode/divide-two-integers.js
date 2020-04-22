ad3344a3a755fb308837ac86af57000d1d68f318
陈星亮 <869997202@qq.com>
2020-04-22 17:30:57 +0800

    解决一出问题

--- leetcode/divide-two-integers.js
+++ leetcode/divide-two-integers.js
@@ -4,14 +4,19 @@
  * @return {number}
  */
 var divide = function(dividend, divisor) {
+    let INT_MIN = (-2) ** 31;
+    let INT_MAX = 2 ** 31 - 1;
     if (dividend === 0) {
         return 0;
     }
-    if (divisor === 1) {
-        return dividend;
-    }
     if (dividend === divisor) {
         return 1;
+    }
+    if (divisor === 1) {
+        return dividend < INT_MIN || dividend > INT_MAX ? INT_MAX : dividend;;
+    }
+    if (divisor === -1) {
+        return -dividend < INT_MIN || -dividend > INT_MAX ? INT_MAX : -dividend;;
     }
     if (-dividend === divisor || dividend === -divisor) {
         return -1;
@@ -24,32 +29,31 @@ var divide = function(dividend, divisor) {
     }
     dividend = Math.abs(dividend);
     divisor = Math.abs(divisor);
-    let temp = divisor,
-        sum = 1,
-        res = 0;
     if (dividend < divisor) {
         return 0;
     }
-    while ((temp <<= 1) < dividend) {
+    let temp = divisor,
+        sum = 1,
+        res = 0;
+    while (temp << 1 < dividend) {
+        if (temp << 1 < 0) {
+            break;
+        }
+        temp <<= 1
         sum <<= 1;
     }
-    if (temp === dividend) {
-        sum <<= 1;
-        res = sum;
-    } else {
-        temp >>= 1;
-        while (dividend >= divisor) {
-            dividend -= temp;
-            res += sum;
-            while (temp > dividend) {
-                temp >>= 1
-                sum >>= 1;
-            }
+    while (dividend >= divisor) {
+        dividend -= temp;
+        res += sum;
+        while (temp > dividend) {
+            temp >>= 1
+            sum >>= 1;
         }
     }
-    
+    if(-res < INT_MIN) return INT_MAX;
+    if(res > INT_MAX) return INT_MAX;
     return sign ? res : -res;
 };
 
-let test = divide(150, 10);
+let test = divide(-2147483648, -1);
 console.log(test);

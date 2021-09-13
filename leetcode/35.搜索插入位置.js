36ab14108a6aaf7695923fd6cbe7ee290d1a322a
chenxingliang <869997202@qq.com>
2021-09-13 19:06:20 +0800

    搜索插入位置

--- leetcode/35.搜索插入位置.js
+++ leetcode/35.搜索插入位置.js
@@ -11,7 +11,44 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-
+  if (target <= nums[0]) {
+    return 0;
+  }
+  if (target > nums[nums.length - 1]) {
+    return nums.length;
+  }
+  if (target === nums[nums.length - 1]) {
+    return nums.length - 1;
+  }
+  let max = nums.length;
+  let min = 0;
+  let middle = Math.floor((max - min) / 2);
+  while (true) {
+    if (target === nums[middle]) {
+      return middle;
+    } else if (target < nums[middle] && target > nums[middle - 1]) {
+      return middle;
+    } else if (target < nums[middle] && target === nums[middle - 1]) {
+      return middle - 1;
+    } else if (target > nums[middle] && target <= nums[middle + 1]) {
+      return middle + 1;
+    } else if (target < nums[middle]) {
+      max = middle - 1;
+      middle = Math.floor((max - min) / 2) + min;
+    } else if (target > nums[middle]) {
+      min = middle + 1
+      middle = Math.floor((max - min) / 2) + min;
+    } 
+  }
 };
 // @lc code=end
-
+// console.log(searchInsert([1,3,5,6], 5))
+// console.log(searchInsert([1,3,5,6], 2))
+// console.log(searchInsert([1,3,5,6], 7))
+// console.log(searchInsert([1,3,5,6], 0))
+// console.log(searchInsert([1], 0))
+// console.log(searchInsert([1,4,6,7,8,9], 7))
+// console.log(searchInsert([1,3], 3))
+// console.log(searchInsert([1,2,3,4,5,10], 2))
+// console.log(searchInsert([1,4,6,7,8,9], 6))
+console.log(searchInsert([1,3,5,6], 2))

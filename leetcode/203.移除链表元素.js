6c8e782a7cc560d433c7aafb33137ad887c43ec8
chenxingliang <869997202@qq.com>
2021-10-29 17:38:54 +0800

    移除链表元素

---
+++ leetcode/203.移除链表元素.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=javascript
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  let prev = new ListNode();
+  prev.next = head;
+  const res = prev;
+
+  while(prev && prev.next) {
+    if (prev.next.val === val) {
+      prev.next = prev.next.next;
+    } else {
+      prev = prev.next;
+    }
+  }
+
+  return res.next;
+};
+// @lc code=end
+
